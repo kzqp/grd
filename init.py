544a5bea058aec6fec0f849c0921114ecee83692
Kevin Shaw <klshaw@gcc.edu>
2023-09-13 10:11:18 -0400

    Expand init functions for username

--- init.py
+++ init.py
@@ -1,43 +1,50 @@
+"""Gradebook initialization functions"""
+
 from datetime import datetime
 import os
-import student
 import grd_io
 import data
 import user
 
 def read_settings():
-  grd_io.read_config_file()
+    """read variables from config file"""
+    grd_io.read_config_file()
 
 def init():
-  year = datetime.now().year
-  print(f'Let\'s get started for {year}.')
-  print('Enter your student list in the form StudentID:Last Name:First Name.\n'
-        'Enter a blank line when finished.')
-  string = 'x'
-  while string != '':
-    string = input('Student: ')
-    if string != '':
-      try:
-        grd_io.parse_student(string)
-        print(f'Added {data.gradebook[len(data.gradebook)-1].fn}'
-              f' {data.gradebook[len(data.gradebook)-1].ln}'
-              f' ({data.gradebook[len(data.gradebook)-1].id}).')
-      except Exception as e:
-        print(e)
-        print('I think you may have made a mistake....')
-  print(f'Added {len(data.gradebook)} students to the gradebook for {year}.')
-  #now to write the file(s)
-  print('Writing student data to disk...', end='')
-  grd_io.writeFile(f'.grd-{year}')
-  data.RegisteredUser.current_grade_book = os.getcwd() + '/.grd-' + str(year)
-  grd_io.set_current_gradebook()
-  print('done!')
-  #and the history file, someday
+    """initialize gradebook"""
+    year = datetime.now().year
+    print(f'Let\'s get started for {year}.')
+    print('Enter your student list in the form StudentID:Username:'
+          'Last Name:First Name.\n'
+          'Enter a blank line when finished.')
+    string = 'x'
+    while string != '':
+        string = input('Student: ')
+        if string != '':
+            try:
+                grd_io.parse_student(string)
+                print(f'Added '
+                      f'{data.gradebook[len(data.gradebook)-1].first_name}'
+                      f' {data.gradebook[len(data.gradebook)-1].last_name}'
+                      f' ({data.gradebook[len(data.gradebook)-1].student_ID}'
+                      f', {data.gradebook[len(data.gradebook)-1].username}).')
+            except Exception as e:
+                print(e)
+                print('I think you may have made a mistake....')
+    print(f'Added {len(data.gradebook)} students to the gradebook for {year}.')
+    #now to write the file(s)
+    print('Writing student data to disk...', end='')
+    grd_io.writeFile(f'.grd-{year}')
+    data.RegisteredUser.current_grade_book = os.getcwd() + '/.grd-' + str(year)
+    grd_io.set_current_gradebook()
+    print('done!')
+    #and the history file, someday
 
 def gather_user_info():
-  print('Let\'s collect some basic information to get started.')
-  fn = input('First Name: ')
-  ln = input('Last Name: ')
-  em = input('email : ')
-  data.RegisteredUser = user.user(fn, ln, em)
-  grd_io.write_config_file()
+    """collect user info, first run"""
+    print('Let\'s collect some basic information to get started.')
+    first_name = input('First Name: ')
+    last_name = input('Last Name: ')
+    email = input('email : ')
+    data.RegisteredUser = user.user(first_name, last_name, email)
+    grd_io.write_config_file()
